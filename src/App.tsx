8eccf84c3181a22ad5d83ef07c3f4868c5963c54
Nick-Lemy <n.kayiranga@alustudent.com>
2025-06-28 16:20:07 +0200

    ci: setup project from scratch

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold flex items-center justify-center py-10'>Smart School Blog</h1>
+    </>
+  )
+}
+
+export default App
