9c1c526bdf63ca74fede1d413e32caac26baf168
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-13 17:00:34 +0200

    fix: remove unnecesarry components

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,5 +1,69 @@
-import { RegisterForm } from "@/components/register-form";
+"use client";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import GoogleIcon from "@/components/icons/GoogleIcon";
+import { useRouter } from "next/navigation";
 
-export default function RegisterPage() {
-  return <RegisterForm />;
+export default function RegisterForm({
+  className,
+  ...props
+}: React.ComponentProps<"form">) {
+  const router = useRouter();
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Create your account</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter your email below to create your account
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+          </div>
+          <Input id="password" type="password" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Confirmation Password</Label>
+          </div>
+          <Input id="password" type="confirmation_password" required />
+        </div>
+        <Button
+          onClick={() => router.push("/home/dashboard")}
+          type="submit"
+          className="w-full"
+        >
+          Register
+        </Button>
+        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+          <span className="bg-background text-muted-foreground relative z-10 px-2">
+            Or continue with
+          </span>
+        </div>
+        <Button
+          onClick={() => router.push("/home/dashboard")}
+          variant="outline"
+          className="w-full"
+        >
+          <GoogleIcon />
+          Register with Google
+        </Button>
+      </div>
+      <div className="text-center text-sm">
+        Already have an account?{" "}
+        <Link href="/auth/login" className="underline underline-offset-4">
+          Login
+        </Link>
+      </div>
+    </form>
+  );
 }
