5be3789ab8da5c22c4b25b484549e50815d4a63f
Nick-Lemy <n.kayiranga@alustudent.com>
2025-08-01 15:58:48 +0200

    chore: modify style dismatch

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -175,7 +175,7 @@ export default function RegisterPage() {
                 name="role"
                 value={formData.role}
                 onChange={handleChange}
-                className="block w-full text-white px-3 py-2 border border-gray-300 rounded-md bg-transparent shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                className="block w-full *:bg-gray-600 text-white px-3 py-2 border border-gray-300 rounded-md bg-transparent shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
               >
                 <option value="student">Student</option>
                 <option value="teacher">Teacher</option>
