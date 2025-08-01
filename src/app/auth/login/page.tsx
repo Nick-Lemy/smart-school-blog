f768af9dcb0140af7fbfbd508e60196427ceebf0
Nick-Lemy <n.kayiranga@alustudent.com>
2025-08-01 15:40:31 +0200

    hotfix: white text on login as it is on register page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -80,7 +80,7 @@ export default function LoginPage() {
                 }
                 required
                 placeholder="name@school.edu"
-                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                className="appearance-none text-white block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
               />
             </div>
 
@@ -112,7 +112,7 @@ export default function LoginPage() {
                 }
                 required
                 placeholder="Enter your password"
-                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                className="appearance-none text-white block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
               />
             </div>
           </div>
